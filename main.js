12bbccf16b069d7abb03896a86d66196f6fd105a
diasEduardo <eduardo_dududex@hotmail.com>
2018-12-23 21:25:50 -0200

    telegram bot added

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var Discord = require('discord.io');
 //var logger = require('winston');
 var auth = require('./auth.json');
 
+
+
 // Configure logger settings
 //logger.remove(logger.transports.Console);
 //logger.add(logger.transports.Console, {
@@ -9,20 +11,20 @@ var auth = require('./auth.json');
 //});
 //.level = 'debug';
 
-// Initialize Discord Bot
-var bot = new Discord.Client({
+// Initialize Discord bot
+var discordBot = new Discord.Client({
    token: auth.token,
    autorun: true
 });
-bot.on('ready', function (evt) {
+discordBot.on('ready', function (evt) {
     console.log('Connected');
     console.log('Logged in as: ');
-    console.log(bot.username + ' - (' + bot.id + ')');
+    console.log(discordBot.username + ' - (' + discordBot.id + ')');
 });
 
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
+discordBot.on('message', function (user, userID, channelID, message, evt) {
+    // Our discordBot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
         
     if (message.substring(0, 1) == '\\') {
@@ -34,24 +36,9 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             // !ping
             case 'on':
             case 'lancha':
-                var usersOn = getAllUsersOn();
-                msg = "";
-                console.log(usersOn);
-                if(isEmpty(usersOn)){
-                    msg = "Não há pessoas nos canais de voz"
-                }else {
-                    for (channel in usersOn){
-                        msg += "Canal " + bot.channels[channel]["name"] + "\n";
-                        for (member in usersOn[channel]){
-                            name = usersOn[channel][member]["username"];
-                            if( usersOn[channel][member]["nick"] != null ) name = usersOn[channel][member]["nick"]; 
-                            msg += "\t" + name + " ( "+usersOn[channel][member]["username"]+"#"+usersOn[channel][member]["discriminator"]+" )\n";
-                            console.log();
-                        }
-                        msg += "\n";
-                    }
-                }
-                bot.sendMessage({
+                
+                msg = makeUsersOnText();                
+                discordBot.sendMessage({
                     to: channelID,
                     message: msg
                 });
@@ -61,8 +48,25 @@ bot.on('message', function (user, userID, channelID, message, evt) {
      }
 });
 
-function makeUsersOnJSON(){
 
+function makeUsersOnText(){
+    msg = "";
+    var usersOn = getAllUsersOn();
+    if(isEmpty(usersOn)){
+        msg = "Não há pessoas nos canais de voz"
+    }else {
+        for (channel in usersOn){
+            msg += "Canal " + discordBot.channels[channel]["name"] + "\n";
+            for (member in usersOn[channel]){
+                name = usersOn[channel][member]["username"];
+                if( usersOn[channel][member]["nick"] != null ) name = usersOn[channel][member]["nick"]; 
+                msg += "\t" + name + " ( "+usersOn[channel][member]["username"]+"#"+usersOn[channel][member]["discriminator"]+" )\n";
+                console.log();
+            }
+            msg += "\n";
+        }
+    }
+    return msg;
 }
 
 function getAllUsersOn(){
@@ -78,19 +82,19 @@ function getAllUsersOn(){
 }
 
 function getUsersOn(channel){
-    var chan = bot.channels[channel];
+    var chan = discordBot.channels[channel];
     var members = chan.members;
     var temp = {};
     
     for (member in members) {
             user = {};
             user["user_id"] = member;
-            user["nick"] = bot.servers[chan.guild_id].members[member]["nick"];
-            user["username"] = bot.users[member]["username"];
-            user["discriminator"] = bot.users[member]["discriminator"];
+            user["nick"] = discordBot.servers[chan.guild_id].members[member]["nick"];
+            user["username"] = discordBot.users[member]["username"];
+            user["discriminator"] = discordBot.users[member]["discriminator"];
             user["mute"] = members[member]["mute"] || members[member]["self_mute"];
             user["deaf"] = members[member]["deaf"] || members[member]["self_deaf"];
-            user["game"] = bot.users[member]["game"];
+            user["game"] = discordBot.users[member]["game"];
             temp[member] = user;
         }
     return temp;
@@ -98,16 +102,35 @@ function getUsersOn(channel){
 
 function getVoiceChannels(){
     var voiceChannels = [];
-    for (channel in bot.channels) {
-          if (bot.channels[channel].type == 2){
+    for (channel in discordBot.channels) {
+          if (discordBot.channels[channel].type == 2){
             voiceChannels.push(channel);
           }
     }
 
     return voiceChannels;
 }
-60
+
 
 function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
+    return Object.keys(obj).length === 0;
 }
+
+//Initialize Telegram bot
+const TelegramBot = require( `node-telegram-bot-api` )
+const teleBot = new TelegramBot( auth.telegram, { polling: true } )
+teleBot.onText('/start', (msg) => {
+    
+     teleBot.sendMessage(msg.chat.id,"ALOOOU");
+     
+});
+teleBot.onText(/\/on/, (msg) => {
+    
+     teleBot.sendMessage(msg.chat.id,makeUsersOnText());
+     
+});
+teleBot.onText(/\/lancha/, (msg) => {
+    
+     teleBot.sendMessage(msg.chat.id,makeUsersOnText());
+     
+});
